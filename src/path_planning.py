e1663f0bd0d1045a69c07ec926d30956eb77e93b
BarzinM <Barzin.M@gmail.com>
2015-10-03 21:58:54 -0400

    started a new path planning method

--- src/path_planning.py
+++ src/path_planning.py
@@ -1,4 +1,5 @@
 from numpy import cos, sin, pi, arcsin
+nan = float('nan')
 
 
 class GapFinding(object):
@@ -6,78 +7,86 @@ class GapFinding(object):
     """docstring for ClassName"""
 
     def __init__(self, safe_radius):
-        self.safe_radius = safe_radius
-        self.safe_gap = safe_radius**2 * 4
-        self.distance_range = 15
+        self.safe_radius = safe_radius * 1.0
+        self.safe_gap = self.safe_radius**2 * 4
+        self.distance_range = 15.0
+        self.readings_polar = []
 
-    def polarToCartesian(self, distance, angle):
+    def setDistanceRange(self, distance_range):
+        self.distance_range = distance_range
 
-        self.number_of_readings = len(distance)
-        # variable initialization
-        i = 1
-        x = [0 for reading in distance]
-        y = [0 for reading in distance]
+    def polarToCartesian(self):
+        x_cordinate = [0.0 for reading in self.readings_polar]
+        y_cordinate = [0.0 for reading in self.readings_polar]
 
-        x[0] = distance[0] * cos(angle[0])
-        y[0] = distance[0] * sin(angle[0])
+        for i in range(len(self.readings_polar)):
+            x_cordinate[i] = self.readings_polar[i][0] * cos(self.readings_polar[i][1])
+            y_cordinate[i] = self.readings_polar[i][0] * sin(self.readings_polar[i][1])
 
-        while i < self.number_of_readings:
-            x[i] = distance[i] * cos(angle[i])
-            y[i] = distance[i] * sin(angle[i])
-            i += 1
-
-        self.first_obstacle_end = self.__findFirstGap(x, y)
-        print 'first obstacle end:', self.first_obstacle_end
-
-        return x, y
+        return x_cordinate, y_cordinate
 
     def __findFirstGap(self, x, y):
         i = 1
         first_obstacle_end = -1
 
         while first_obstacle_end < 0 and i < self.number_of_readings:
+
             gap_x = x[i] - x[i - 1]
             gap_y = y[i] - y[i - 1]
             gap = gap_x**2 + gap_y**2
+            # print gap, i
             if gap > self.safe_gap:
+                # print 'TTTTTTTTTThe gap is', gap, self.safe_gap, i - 1
                 return i - 1
             i += 1
 
-        return -1
+        return self.number_of_readings - 1
 
     def filterReadings(self, distances, angles):
-        self.number_of_readings = len(distances)
-        for i in range(self.number_of_readings):
-            if distances[i] > .95 * self.distance_range:
-                distances[i] = None
-                angles[i] = None
+        number_of_all_readings = len(distances)
+        inner_bound = self.safe_radius * .7
+        for i in range(number_of_all_readings):
+            if distances[i] > .95 * self.distance_range or distances[i] < inner_bound:
+                distances[i] = -1
+                # angles[i] = []
+            # else:
+                # print 'found one'
 
-        distances = [distance for distance in distances if distance != None]
-        angles = [angle for angle in angles if angle != None]
+        # print distances
+        self.readings_polar = [[distances[i], angles[i]] for i in range(number_of_all_readings) if distances[i] != -1]
+        # print len(self.readings_polar),self.readings_polar
+        self.number_of_readings = len(self.readings_polar)
+        # distances = [distance for distance in distances if distance != None]
+        # angles = [angle for angle in angles if angle != None]
+        # self.reading_distances = distances
         return distances, angles
 
     def findObstacleLimits(self, x, y):
-        print 'number of readings:', len(x)
+        print 'mmmmmmmmmmmmmmmmmmmmmmmmmmm'
+        # print len(x)
+        self.first_obstacle_end = self.__findFirstGap(x, y)
+        print self.first_obstacle_end
+        # print 'number of readings_polar:', len(x)
 
         def isSafe(index_1, index_2):
             gap_x = x[index_1] - x[index_2]
             gap_y = y[index_1] - y[index_2]
             gap = gap_x**2 + gap_y**2
-            print 'the gap is', gap
+            # print 'the gap is', gap, index_1, index_2
             return gap > self.safe_gap
 
         number_of_obstacles = 0
         obstacle_end_index = self.first_obstacle_end
         self.obstacle_limits = []
-        obstacle_start_index = obstacle_end_index + 1
+        obstacle_start_index = (obstacle_end_index + 1) % self.number_of_readings
         obstacle_end_index = None
 
         while not obstacle_end_index == (self.first_obstacle_end % self.number_of_readings):
-            print 'explorriing new obstacle with start index:', obstacle_start_index
+            # print 'explorriing new obstacle with start index:', obstacle_start_index
             number_of_obstacles += 1
-            if number_of_obstacles > 4:
-                print 'number of obstacles got more than 2'
-                break
+            # if number_of_obstacles > 4:
+            # print 'number of obstacles got more than 2'
+            #     break
             self.obstacle_limits.append([obstacle_start_index])
 
             obstacle_vertex_index = obstacle_start_index
@@ -85,11 +94,11 @@ class GapFinding(object):
             found_obstacle_end = False
 
             scanner = obstacle_vertex_index
-            control_val = 0
+            # control_val = 0
             while not found_obstacle_end:
                 scanner = (self.number_of_readings + self.first_obstacle_end) % self.number_of_readings
                 while True:
-                    print 'Evaluating indexes:', obstacle_vertex_index, scanner
+                    # print 'Evaluating indexes:', obstacle_vertex_index, scanner
                     if scanner == obstacle_vertex_index:
                         obstacle_end_index = scanner
                         found_obstacle_end = True
@@ -98,16 +107,16 @@ class GapFinding(object):
                         break
                     if not isSafe(scanner, obstacle_vertex_index):
                         obstacle_vertex_index = (obstacle_vertex_index + 1) % self.number_of_readings
-                        print 'found new vertex', obstacle_vertex_index
+                        # print 'found new vertex', obstacle_vertex_index
                         break
                     else:
                         scanner = (scanner - 1) % self.number_of_readings
 
-                control_val += 1
-                if control_val > 100:
-                    print 'DIDN"T GET OUT OF THIS LOOP'
-                    break
-            print 'obstacle end is:', obstacle_end_index
+                # control_val += 1
+                # if control_val > 100:
+                # print 'DIDN"T GET OUT OF THIS LOOP'
+                #     break
+            # print 'obstacle end is:', obstacle_end_index
             self.obstacle_limits[-1].append(obstacle_end_index)
         # self.obstacle_limits[1:]
 
@@ -115,14 +124,25 @@ class GapFinding(object):
         pass
 
     def defineSubgoals(self, distances, angles):
-        number_of_obstacles = len(self.obstacle_limits) / 2
-        subgoals_distance = subgoals_angle = [[None,None] for i in range(number_of_obstacles)]
+        number_of_obstacles = len(self.obstacle_limits)
+        subgoals_angle = [[[], []] for i in range(number_of_obstacles)]
+        subgoals_distance = subgoals_angle
+        subgoal_temp = [[], []]
         for i in range(number_of_obstacles):
-            obstacle_vertices = range(self.obstacle_limits[2*i],self.obstacle_limits[2*i+1])
+            # print 'evaluating new obstacle'
+            obstacle_vertices = range(self.obstacle_limits[i][0], self.obstacle_limits[i][1] + 1)
             for obstacle_vertex in obstacle_vertices:
-                subgoal_clockwise = angles[obstacle_vertex] - arcsin(self.safe_radius/distances[obstacle_vertex])
-                subgoal_counter_clockwise = angles[obstacle_vertex] + arcsin(self.safe_radius/distances[obstacle_vertex])
+                # print angles[obstacle_vertex], self.safe_radius, distances[obstacle_vertex], arcsin(self.safe_radius / distances[obstacle_vertex])
+                subgoal_temp[0] = angles[obstacle_vertex] - arcsin(self.safe_radius / distances[obstacle_vertex])
+                subgoal_temp[1] = angles[obstacle_vertex] + arcsin(self.safe_radius / distances[obstacle_vertex])
+
+                subgoals_angle[i][0] = min(subgoal_temp[0], subgoals_angle[i][0])
+                subgoals_angle[i][1] = max(subgoal_temp[1], subgoals_angle[i][1])
+
+        return subgoals_angle
+
                 # subgoals_angle(i) = min([subgoals_angle(i),subgoal])
+
     def selectSubgoal(self, distances, angles, target_distnce, target_theta):
         x, y = self.polarToCartesian(distances, angles)
         self.findObstacleLimits(x, y)
@@ -136,3 +156,67 @@ class GapFinding(object):
         if x_gap**2 + y_gap**2 > safe_gap:
             return True
         return False
+
+
+class SimpleGapFinder(object):
+
+    def __init__(self, safe_radius):
+        self.safe_radius = safe_radius * 1.0
+        self.safe_gap = self.safe_radius**2 * 4
+        self.distance_range = 15.0
+        self.readings_polar = []
+
+    def setDistanceRange(self, distance_range):
+        self.distance_range = distance_range
+
+    def polarToCartesian(self):
+        x_cordinate = [0.0 for reading in self.readings_polar]
+        y_cordinate = [0.0 for reading in self.readings_polar]
+
+        for i in range(len(self.readings_polar)):
+            x_cordinate[i] = self.readings_polar[i][0] * cos(self.readings_polar[i][1])
+            y_cordinate[i] = self.readings_polar[i][0] * sin(self.readings_polar[i][1])
+
+        return x_cordinate, y_cordinate
+
+    def filterReadings(self, distances, angles):
+        number_of_all_readings = len(distances)
+        inner_bound = self.safe_radius * .7
+        for i in range(number_of_all_readings):
+            if distances[i] > .95 * self.distance_range or distances[i] < inner_bound:
+                distances[i] = -1
+                # angles[i] = []
+            # else:
+                # print 'found one'
+
+        # print distances
+        self.readings_polar = [[distances[i], angles[i]] for i in range(number_of_all_readings) if distances[i] != -1]
+        # print len(self.readings_polar),self.readings_polar
+        self.number_of_readings = len(self.readings_polar)
+        # distances = [distance for distance in distances if distance != None]
+        # angles = [angle for angle in angles if angle != None]
+        # self.reading_distances = distances
+        return distances, angles
+
+    def findGaps(self):
+        print self.readings_polar
+        possible_travel = [0.0 for i in range(len(self.readings_polar))]
+        for i in range(len(self.readings_polar)):
+            possible_travel[i] = self.readings_polar[i][0]
+            for j in range(i - 1, -1, -1):
+                angular_difference = self.readings_polar[i][1] - self.readings_polar[j][1]
+                distance = self.readings_polar[i][0] - self.readings_polar[j][0]
+                if  angular_difference > pi / 2:
+                    print 'breakings'
+                    break
+                if abs(distance)>self.safe_radius*2:
+                    continue
+                elif 
+
+            for j in range(i + 1, self.number_of_readings):
+                if self.readings_polar[j][1]-self.readings_polar[j][1] > pi/2:
+                    print 'breaking'
+                    break
+                if abs(self.readings_polar[i][0] - self.readings_polar[j][0])>self.safe_radius*2:
+                    continue
+
--- src/unittests/path_planning_test.py
+++ src/unittests/path_planning_test.py
@@ -1,117 +1,124 @@
 # content of test_sample.py
 import pytest
-from numpy import pi
+from numpy import pi, load, linspace, sin, cos
 from sys import path
+import glob
+import matplotlib.pyplot as plt
 path.append('..')
-from path_planning import GapFinding
+from path_planning import GapFinding, SimpleGapFinder
 
 acceptable_error = .001
 
 
 @pytest.fixture
-def gap_finder():
+def simple_lidar():
     return GapFinding(1)
 
 
-def test_polarToCartesian(gap_finder):
-    x, y = gap_finder.polarToCartesian([1, 1], [0, pi / 2])
-    assert x[0] == 1
-    assert y[0] == 0
+@pytest.fixture
+def actual_lidar():
+    finder = GapFinding(.4)
+    finder.setDistanceRange(4.0)
+    return finder
+
+@pytest.fixture
+def simple_gap_finder():
+    finder = SimpleGapFinder(.4)
+    finder.setDistanceRange(4.0)
+    return finder
+# def test_polarToCartesian(simple_lidar):
+#     x, y = simple_lidar.polarToCartesian([1, 1], [0, pi / 2])
+#     assert x[0] == 1
+#     assert y[0] == 0
 
 
-def test_filterReadings(gap_finder):
+def test_filterReadings(simple_lidar):
     readings = 6
     distances = [20 for i in range(readings)]
-    distances[3] = 3
     angles = [10 * i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
+    distances[3] = 3
+    distances, angles = simple_lidar.filterReadings(distances, angles)
 
 
-def test_findObstacleLimits1(gap_finder):
-    print 'Std Outputs:'
+def test_findObstacleLimits1(simple_lidar):
     readings = 6
     distances = [20 for i in range(readings)]
-    distances[1] = 9
-    distances[2] = 9
-    distances[3] = 3
-    distances[4] = 14
-    # distances[5] = 9
     angles = [10 * i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
-    x, y = gap_finder.polarToCartesian(distances, angles)
-    print 'Distances and angles:', distances, angles
-    gap_finder.findObstacleLimits(x, y)
-    assert gap_finder.obstacle_limits == [[2, 2],[ 3, 3], [0, 1]]
+
+    distances[1] = 8.0
+    distances[2] = 8.0
+    distances[3] = 2.0
+    distances[4] = 14.0
+
+    simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    assert simple_lidar.obstacle_limits == [[2, 2], [3, 3], [0, 1]]
 
 
-def test_findObstacleLimits2(gap_finder):
-    print 'Std Outputs:'
+def test_findObstacleLimits2(simple_lidar):
     readings = 6
     distances = [20 for i in range(readings)]
 
     distances[3] = 2
     distances[4] = 2
     angles = [10 * i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
-    x, y = gap_finder.polarToCartesian(distances, angles)
-    print 'Distances and angles:', distances, angles
-    gap_finder.findObstacleLimits(x, y)
-    assert gap_finder.obstacle_limits == [[0, 1]]
+    distances, angles = simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    assert simple_lidar.obstacle_limits == [[0, 1]]
 
 
-def test_findObstacleLimits3(gap_finder):
-    print 'Std Outputs:'
+def test_findObstacleLimits3(simple_lidar):
     readings = 6
     distances = [20 for i in range(readings)]
 
     distances[3] = 3
     # distances[4] = 3
     angles = [10 * i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
-    x, y = gap_finder.polarToCartesian(distances, angles)
-    print 'Distances and angles:', distances, angles
-    gap_finder.findObstacleLimits(x, y)
-    assert gap_finder.obstacle_limits == [[0, 0]]
+    distances, angles = simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    assert simple_lidar.obstacle_limits == [[0, 0]]
 
 
-def test_findObstacleLimits4(gap_finder):
-    print 'Std Outputs:'
+def test_findObstacleLimits4(simple_lidar):
     readings = 6
     distances = [20 for i in range(readings)]
-    # distances[1] = 9
+    angles = [10 * i * pi / 180 for i in range(readings)]
+
     distances[2] = 9
     distances[3] = 9
     distances[4] = 9
-    # distances[5] = 9
-    angles = [10 * i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
-    x, y = gap_finder.polarToCartesian(distances, angles)
-    print 'Distances and angles:', distances, angles
-    gap_finder.findObstacleLimits(x, y)
-    assert gap_finder.obstacle_limits == [[0, 2]]
+
+    distances, angles = simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    assert simple_lidar.obstacle_limits == [[0, 2]]
 
 
-def test_findObstacleLimits5(gap_finder):
-    print 'Std Outputs:'
+def test_findObstacleLimits5(simple_lidar):
     readings = 6
     distances = [20 for i in range(readings)]
+    angles = [10 * i * pi / 180 for i in range(readings)]
+
     distances[1] = 9
     distances[2] = 9
     distances[3] = 3
     distances[4] = 14
     distances[5] = 9
-    angles = [10 * i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
-    x, y = gap_finder.polarToCartesian(distances, angles)
-    print 'Distances and angles:', distances, angles
-    gap_finder.findObstacleLimits(x, y)
-    assert gap_finder.obstacle_limits == [[2, 2], [3, 3], [4, 4], [0, 1]]
+
+    distances, angles = simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    assert simple_lidar.obstacle_limits == [[2, 2], [3, 3], [4, 4], [0, 1]]
 
 
-def test_findObstacleLimits6(gap_finder):
-    print 'Std Outputs:'
+def test_findObstacleLimits6(simple_lidar):
     readings = 100
     distances = [20 for i in range(readings)]
+    angles = [i * pi / 180 for i in range(readings)]
+
     for i in range(20, 30):
         distances[i] = 9
 
@@ -120,10 +127,63 @@ def test_findObstacleLimits6(gap_finder):
     distances[3] = 3
     distances[4] = 14
     distances[5] = 9
-    angles = [i * pi / 180 for i in range(readings)]
-    distances, angles = gap_finder.filterReadings(distances, angles)
-    x, y = gap_finder.polarToCartesian(distances, angles)
-    print 'Distances and angles:', distances, angles
-    gap_finder.findObstacleLimits(x, y)
-    print gap_finder.obstacle_limits
 
+    distances, angles = simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    assert simple_lidar.obstacle_limits == [[2, 2], [3, 3], [4, 14], [0, 1]]
+
+
+def test_defineSubgoals(simple_lidar):
+    readings = 6
+    distances = [20 for i in range(readings)]
+    angles = [10 * i * pi / 180 for i in range(readings)]
+
+    distances[3] = 9
+    distances[4] = 3
+
+    distances, angles = simple_lidar.filterReadings(distances, angles)
+    x, y = simple_lidar.polarToCartesian()
+    simple_lidar.findObstacleLimits(x, y)
+    print simple_lidar.defineSubgoals(distances, angles)
+
+
+def ntest_lidarSamples(actual_lidar):
+    for file in glob.glob("*.npy"):
+        # print test
+        data = load(file)
+        print file
+        distances = data
+        angles = linspace(-pi / 4, 5 * pi / 4, len(distances))
+        actual_lidar.filterReadings(distances, angles)
+        x, y = actual_lidar.polarToCartesian()
+        actual_lidar.findObstacleLimits(x, y)
+        # print data
+        # print actual_lidar.readings_polar
+
+        f0 = plt.figure()
+        ax0 = f0.add_subplot(111)
+        ax0.plot(x, y, 'r.')
+        ax0.plot(0, 0, 'ko', markersize=10)
+        for i in range(len(actual_lidar.obstacle_limits)):
+            distance = actual_lidar.readings_polar[actual_lidar.obstacle_limits[i][0]][0]
+            theta = actual_lidar.readings_polar[actual_lidar.obstacle_limits[i][0]][1]
+            x1 = distance * cos(theta)
+            y1 = distance * sin(theta)
+            distance = actual_lidar.readings_polar[actual_lidar.obstacle_limits[i][1]][0]
+            theta = actual_lidar.readings_polar[actual_lidar.obstacle_limits[i][1]][1]
+            x2 = distance * cos(theta)
+            y2 = distance * sin(theta)
+            # print actual_lidar.obstacle_limits,x1, x2
+            # xdata =
+            # print xdata
+            ax0.plot([x1, x2], [y1, y2],'b', linewidth=3)
+        ax0.axis('equal')
+        plt.draw()
+        plt.pause(.1)
+        raw_input("<Hit Enter To Close>")
+        plt.close(f0)
+
+def test_findGaps(simple_gap_finder):
+    simple_gap_finder.filterReadings([1,1],[.17,.34])
+    simple_gap_finder.findGaps()
